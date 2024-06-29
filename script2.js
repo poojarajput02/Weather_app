95a3cd8abf1e6b1dc992b3fa576de0ed929a8478
pooja rajput <108475487+poojarajput02@users.noreply.github.com>
2024-06-30 00:36:48 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,5 @@
-const url =
-	'https://api.openweathermap.org/data/2.5/weather';
-const apiKey =
-	'f00c38e0279b7bc85480c3fe775d518c';
+const url ='https://api.openweathermap.org/data/2.5/weather';
+const apiKey ='f00c38e0279b7bc85480c3fe775d518c';
 
 $(document).ready(function () {
 	weatherFn('Delhi');
